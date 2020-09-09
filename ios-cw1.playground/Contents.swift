cd6d1bd0988a5da6cd80ea06406ad7d3dacbf40e
Fatmah-alrashidi-C55 <66441802+Fatmah-alrashidi-481@users.noreply.github.com>
2020-09-09 20:22:17 +0300

    done

---
+++ ios-cw1.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var str = "Hello, playground"
+let name = "fatmah"
+var age = 16
+var height = 160
+var hobby1 = "basktball"
+var hobby2 = "football"
+
+print ("سلام عليكم انا اسمي \(name) و عمري \(age) وطولي \(height)و هواياتي \(hobby1 + "," + hobby2)")
+
+
+let firendname = "noor"
+var firendage = 16
+var firendheight = 165
+var firendhobby1 = "coding"
+var firendhobby2 = "football"
+
+print ("سلام عليكم اسم رفيجتي \(firendname)و عمرها \(firendage)و هواياتها \(firendhobby1 + "," + firendhobby2)")
+
+let agediffrence = firendage - age
+
+firendage == age
+
+hobby2 == firendhobby2
